b17a150893aed64a72fd673f5c4aa353dbcf0514
Alexandre Setta <alexsetta@gmail.com>
2022-05-16 17:48:27 -0300

    teste de  página

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 ﻿function Home() {
-	return <div><h1 align="center">Acesse <a href="https://paradoxofinal.com.br">Paradoxo Final</a> para mais conteúdo.</h1></div>
+	return <div>
+		<h1>Página inicial</h1>
+		<h3 align="center">Acesse <a href="https://paradoxofinal.com.br">Paradoxo Final</a> para mais conteúdo.</h3>
+		</div>
 }
 
 export default Home
