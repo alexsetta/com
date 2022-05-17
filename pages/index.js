175d67bf2fd9ca50becfca3284964480d0674ebe
Alexandre Setta <alexsetta@gmail.com>
2022-05-17 08:05:06 -0300

    nova página

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
-﻿function Home() {
-	return <div>
-			<h1 align="center">
+﻿import styles from '../styles/Home.module.css'
+
+function Home() {	
+	return <div className={styles.div}>
+			<h1 >
 				<p>Tecnologia, dicas, músicas e tudo<br></br>mais que for importante.</p>
 				<p>Acesse <a href="https://paradoxofinal.com.br">Paradoxo Final</a> para mais conteúdo.</p>
 			</h1>
 		</div>
 }
+
 
 export default Home
